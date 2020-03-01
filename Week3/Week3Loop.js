f07a9b50f93cc8c2ba098f946ed7dd638412b490
AaronSalinasBU <58755799+AaronSalinasBU@users.noreply.github.com>
2020-02-29 19:34:08 -0600

    1st submission

--- Week3/Week3Loop.js
+++ Week3/Week3Loop.js
@@ -1,35 +1,6 @@
-//office item var and loop with a if for counter
-var officeItems = ["stapeler", "monitor", "computer", "desk", "lamp", "computer", "lamp", "stapler", "computer", "computer"]
-var current = null;
-var cnt = 0;
-for (var i = 0; i < officeItems.length; i++) {
-    if (officeItems[i] == "computer") {
-        cnt++;
+var a = document.body.innerHTML += "<h1 id= 'arrayTimes'> Hello World </h1> ";
+function repeatingFun (a){
+    for(var i = 0; i < 10; i++ ){
+
     };
 };
-console.log(cnt)
-
-// 
-var peopleWhoWantToSeeMadMaxFuryRoad = [
-    {
-        name: "Mike",
-        age: 12,
-        gender: "male",
-    },{
-        name: "Madeline",
-        age: 80,
-        gender: "female",
-    },{
-        name: "Cheryl",
-        age: 22,
-        gender: "female",
-    },{
-        name: "Sam",
-        age: 30,
-        gender: "male",
-    },{
-        name: "Suzy",
-        age: 4,
-        gender: "female",
-    }
-]
