625dc717b2893a548bcc31a293594d8d78d4da3f
AaronSalinasBU <58755799+AaronSalinasBU@users.noreply.github.com>
2020-02-29 20:44:28 -0600

    Week 3 Project 1

--- Week3/Week3Loop.js
+++ Week3/Week3Loop.js
@@ -1,6 +1,11 @@
-var a = document.body.innerHTML += "<h1 id= 'arrayTimes'> Hello World </h1> ";
-function repeatingFun (a){
-    for(var i = 0; i < 10; i++ ){
+document.body.innerHTML += "<h1 id= 'arrayTimes'></h1> ";
 
-    };
+var repeat = document.getElementById("arrayTimes");
+const names = ["steve", "larry", "joe", "shirley", "steph", "nate", "rick", "emily"];
+for(var i = 0; i < names.length; i++){
+    var newWord = document.createElement("h1")
+    newWord.textContent = names[i]
+    newWord.style.backgroundColor = "blue"
+    newWord.style.textAlign = "center"
+    repeat.append(newWord)
 };
